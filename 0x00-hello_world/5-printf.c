fdb8785984dee25df16e998acec0d3418183d412
auwallinus <auwallinus@gmail.com>
2022-09-09 14:37:31 -0700

    My firls hello world commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+ #include <stdio.h>
+ /**
+  * main - Prints "with proper grammar, but the out come is
+  *   		   a piece of art, " followed by a new line.
+  *
+  * Return: Always 0.
+  */
+int main(void)
+{
+	printf("with proper grammar, but the out come is a piece of art,\n");
+
+	return (0)
+}
