6813de06998b9fd1acc1e029a0e95bfed0ff145b
auwallinus <auwallinus@gmail.com>
2022-09-10 11:04:39 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,29 +1,19 @@
-#include <stdlib.h>
-#include <time.h>
-include <stdio.h>
+#include <stdio.h>
+
 /**
-* main - print if the number is positive, zero, or negative
+* main - Prints the alphabet.
 *
-* Description: using the main function
-* this program prints "Programming is positive, zero, or negative"
 * Return: 0
 */
 int main(void)
 {
-int n;
-int 1;
+	char alp[26] = "abcdefghijklmopqrstuvwxyz";
+	int i;
 
-srand(time(0));
-n = rand() - REND_MAX / 2;
-l = n % 10;
-
-if (1 > 5)
+	for (i = 0; i < 26; i++)
 {
-	printf("Last digit of %d is %d and is greater than 5\n", l);
+	putchar(alp[i]);
 }
-else if (l == 0)
-}
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-}
+	putchar('\n');
 return (0);
 }
