d1ecb89cf7411f1ca9d8a939162f107c651f2d64
auwallinus <auwallinus@gmail.com>
2022-09-11 07:03:48 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,13 +7,13 @@
 */
 int main(void)
 {
-char ch;
-char CH;
+	char ch;
+	char CH;
 
-for (ch = 'a'; ch <= 'z'; ch++)
-putchar(ch);
-for (CH = 'A'; CH <= 'Z'; CH++)
-putchar(CH);
-putchar('\n');
+	for (ch = 'a'; ch <= 'z'; ch++)
+	    putchar(ch);
+	for (CH = 'A'; CH <= 'Z'; CH++)
+	    putchar(CH);
+	putchar('\n');
 return (0)
 }
