179a09c7483e8e2493e20da8dbd14d7388e9c96e
auwallinus <auwallinus@gmail.com>
2022-09-16 11:02:01 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#includ "main.h"
+
+/**
+ * more_numbers - prints numbers to 14
+ * Return: returns nothing
+ */
+void more_numbers(void)
+{
+	int i, ro;
+
+	for (ro = 0; ro < 10; ro++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
