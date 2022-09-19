8e45543fb03e0dc95fa039da972348daac461746
auwallinus <auwallinus@gmail.com>
2022-09-19 13:59:06 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,20 +2,6 @@
 #include "main.h"
 
 /**
- * puts_half - prints a string
- * @str: string to print 
- * Return: nothing
- */
-void puts_half(char *str)
-{
-	int i, len = _strlen(str);
-
-	for (i = ((len - 1) / 2) + 1; i < len; i++)
-		putchar(*(str + i));
-	putchar(10);
-}
-
-/**
  * _strlen - returns the length of a string
  * @s: string
  * 
