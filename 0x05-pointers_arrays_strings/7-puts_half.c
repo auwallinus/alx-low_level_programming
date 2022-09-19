10191eed73baa273062c8c679939a75f6717f2d0
auwallinus <auwallinus@gmail.com>
2022-09-19 13:27:02 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,20 +5,19 @@
  * @str: string to print 
  * Return: void
  */
-
 void puts_half(char *str)
 {
-	int j, i = 0;
+int j, i = 0;
 
-	while (*(str + i))
-		i++;
-	j = i / 2;
-	if (i % 2)
-		j += 1;
-	while (j < i)
-	{
-		_putchar(*(str + j));
-		j++;
-	}
-	_putchar('\n');
+while (*(str + i))
+	i++;
+j = i / 2;
+if (i % 2)
+	j += 1;
+while (j < i)
+{
+_putchar(*(str + j));
+j++;
+}
+_putchar('\n');
 }
