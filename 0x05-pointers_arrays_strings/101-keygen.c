b6bcfe161462a393915d95a099594a4df1146a8f
auwallinus <auwallinus@gmail.com>
2022-09-20 08:21:23 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,26 +1,25 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "main.h"
 
 /**
- * main - entry point
- * Return: generated password
+ * main - generates random passwords for 101-keygen
+ * Return: Always 0
  */
-
 int main(void)
 {
+	int sum;
 	char c;
-	int x;
 
-	srand(time(0));
-	while (x <= 2645)
+	srand(time(NULL));
+	while (sum <= 2645)
 	{
-		c = rand () & 128;
-		x + = c;
-		putchar (c);
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
 	}
-	putchar (2772 - x);
+	putchar(2772 - sum);
 
 	return (0);
 }
