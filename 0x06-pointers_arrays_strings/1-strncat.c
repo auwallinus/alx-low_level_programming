889441bb5a59ac88822ac718f1085b8b7c190af6
auwallinus <auwallinus@gmail.com>
2022-09-22 11:08:38 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- *_strcat - concatenate two strings but add inputted number of bytes
- *@dest: String to be appended upon
- *@src: String to be completed at end of dest
- *@n:integer parameter to compare index to
- * Return: returns new concanated string
+ * _strncat -concatenate two strings but add inputted number of bytes
+ * @dest: string to be appended upon
+ * @src: string to be completed at end of dest
+ * @n: integer parameter to compare index to
+ * Return: returns new concatenated string
  */
 
-char *_strcat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 
 	int index = 0, dest_len = 0;
@@ -19,4 +19,5 @@ char *_strcat(char *dest, char *src, int n)
 		dest[dest_len++] = src[index];
 
 	return (dest);
+
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-#ifndef MAIN_H
+#ifndf MAIN_H
 #define MAIN_H
 #include <stdio.h>
 /**
@@ -10,8 +10,8 @@ void reverse_array(int *a, int n);
 void print_number(int n);
 void print_buffer(char *b, int size);
 int _strcmp(char *s1, char *s2);
-char *_strcat(char *desst, char *src);
-char *_strncat(char *dest, char *src int n);
+char *_strncat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
