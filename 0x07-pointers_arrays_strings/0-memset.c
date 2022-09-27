7409c1f0cae3906137f95418f22145ad05a5112c
auwallinus <auwallinus@gmail.com>
2022-09-27 02:19:11 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,11 @@
 /**
  *_memset - sets a given amount of bytes to a value at an address
  * 
- * @s: memory location to start at
- * @b: character to write to memory
- * @n: number of bytes to write
+ *@s: memory location to start at
+ *@b: character to write to memory
+ *@n: number of bytes to write
  * 
- * Return: pointer to s
+ *Return: pointer to s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
