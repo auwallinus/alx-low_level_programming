f70d564e33388ef272d8d5b3be33b12957d2542c
auwallinus <auwallinus@gmail.com>
2022-09-28 11:52:38 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -20,7 +20,6 @@ int find_sqrt(int num, int root)
 
 	return (find_sqrt(num, root + 1));
 }
-
 /**
  * _sqrt_recursion - Returns the natural square root of a number.
  * @n: The number to return the square root of. 
@@ -34,5 +33,6 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 		return (-1);
 
-	return (find_sqrt(n, root));
+	return (find_sqrt(n, rootet number
+				);
 }
