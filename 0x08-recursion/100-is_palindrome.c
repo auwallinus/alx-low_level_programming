9156470cffb52dc2f9ffc1bcc967b98652a991e2
auwallinus <auwallinus@gmail.com>
2022-09-28 16:29:24 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,68 +1,47 @@
-/*
- * is_palindrome.c
- *
- *
- * Read an input from stdin, print it back to the user and
- * report if it is a palindrome.
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: string
+ * Rreturn: the length of a string.
  */
 
-#include <stdio.h>
-#include <stdlib.h>
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+)
 
-#define MAXLENGTH 31
-
-/*
-  calculate the length of the string
+/**
+ * comparator - compares each character of the string.
+ * @s: string
+ * @n1: smallest iterator.
+ * @n2: biggest iterator.
+ * Return: .
  */
-int get_string_length(char *str);
 
-/*
-  return 1 if it is palindrome
-  return 0 if it is not palindrome
- */
-int is_palindrome(char *str);
-
-int main(int argc, char *argv[])
+int comparator(char *s, int n1, int n2)
 {
-    char input_string[MAXLENGTH];
-
-    // get user input
-    printf("Enter a string to test: ");
-    scanf("%s", input_string);
-
-    if(is_palindrome(input_string)) {
-	printf("%s is a palindrome\n", input_string);
-    } else {
-	printf("%s is not a palindrome\n", input_string);
-    }
-
-    return 0;
-}
-
-int get_string_length(char *str)
-{
-    int length = 0;
-    // if it is not '\0' keep moving
-    while(*str != '\0') {
-	str++;
-	length++;
-    }
-    return length;
-}
-
-int is_palindrome(char *str)
-{
-    char *head = str;
-    int length = get_string_length(str);
-    char *tail = &str[length - 1];
-
-    // two pointers to check palindrome and no spare space is being used
-    while(head < tail) {
-	if(*head != *tail) {
-	    return 0;
+	if (*(s + n1) == *(s + n2))
+	{
+		if (n1 == n2 || n1 == n2 + 1)
+			return (1);
+		return (0 + comparator(s, n1 + 1, n2 - 1));
 	}
-	head++;
-	tail--;
-    }
-    return 1;
+	return (0);
+}
+
+/**
+ * is_palindrome - detects is a string is palindrome.
+ * @s: string.
+ * Return: 1 if s is a palindrome. 0 if not.
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
