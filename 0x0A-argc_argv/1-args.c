69545ae043de77033499e2ca74015ee1042da6c5
auwallinus <auwallinus@gmail.com>
2022-09-30 14:32:34 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,19 +7,9 @@
  * Author: Auwal Linus - Lagos Intranet.
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char __attribute__((unused__)) *argv[])
 {
-	int i;
-
-	if (argc == 1)
-		printf("%d\n", argc - 1);
-	else
-	{
-		for (i = 0; *argv; i++, argv++)
-			;
-
-		printf("%d\n", i - 1);
-	}
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
