0ff7448e0cd07a666a31ff48f4b7a0c934f3dd6b
auwallinus <auwallinus@gmail.com>
2022-10-04 04:52:31 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -25,8 +25,7 @@ char *argstostr(int ac, char **av)
 		for (j = 0; av[i][j] != '\0'; j++)
 			c++;
 		c++;
-	}	
-	aout = malloc((c + 1) * sizeof(char));
+	}	aout = malloc((c + 1) * sizeof(char));
 
 	if (aout == NULL)
 	{
