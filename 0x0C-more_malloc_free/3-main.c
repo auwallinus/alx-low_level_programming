430069ef317b9c4bf1d8aaa3a07d89310690775b
auwallinus <auwallinus@gmail.com>
2022-10-06 05:59:58 -0700

    3-array_range.c

--- 0x0C-more_malloc_free/3-main.c
+++ 0x0C-more_malloc_free/3-main.c
@@ -8,8 +8,8 @@
  * @buffer: the address of memory to print
  * @size: the size of the memory to print
  *
- */
  * Return: Nothing.
+ */
 void simple_print_buffer(int *buffer, unsigned int size)
 {
     unsigned int i;
