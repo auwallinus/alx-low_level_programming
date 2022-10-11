4635c59ad36ce04663e842a339ab9cc2577e2062
auwallinus <auwallinus@gmail.com>
2022-10-11 04:52:35 -0700

    1-pi.h

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,13 @@
+#ifndef PI_H
+#define PI_H
+
+/*
+ * JIGJIGA, NIGERIA
+ * Auth: Auwal Linus
+ * Desc: Header file that defines a macro named PI
+ * 	 as an abbreviation for the token 3.14159265359.
+ */
+
+#define PI 3.14159265359
+
+#endif
