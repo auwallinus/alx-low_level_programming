777894ef85ebd0b34a9bc71dab41a12cdcb0a772
auwallinus <auwallinus@gmail.com>
2022-10-11 05:04:35 -0700

    1-pi.h

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -5,7 +5,7 @@
  * JIGJIGA, NIGERIA
  * Auth: Auwal Linus
  * Desc: Header file that defines a macro named PI
- * 	 as an abbreviation for the token 3.14159265359.
+ * as an abbreviation for the token 3.14159265359.
  */
 
 #define PI 3.14159265359
