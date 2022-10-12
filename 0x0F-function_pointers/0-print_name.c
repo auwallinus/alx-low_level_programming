ebbd7808514e38e217a8532acc6823c656913c3a
auwallinus <auwallinus@gmail.com>
2022-10-12 05:33:20 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,8 +4,11 @@
  * print_name - Prints a name from a function pointer
  * @name: char string
  * @f: A pointer to a function that prints a name.
+ *
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (f == NULL)
+		return;
 	f(name);
 }
