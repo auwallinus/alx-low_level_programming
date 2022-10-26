37c03afb3c97a88c90e25e1e734513b84716efcc
Auwal <auwallinus@gmail.com>
2022-10-26 02:31:33 -0700

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,52 +1,33 @@
+#include <stdlib.h>
 #include "lists.h"
-
-
-static listint_t *new_node(const int n);
 /**
- * add_nodeint_end - add a node to the end of a list.
- * @head: pointer of apointer to the head of a list.
- * @n: number value to set the new node to.
- *
- * Return: pointer to the head of a list.
+ * add_nodeint_end - add new node to list at end
+ * @head: head node
+ * @n: node data
+ * Return: address of new node
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new, *temp;
+	listint_t *node, *last;
 
-	if (!(*head))
-	{
-		*head = new_node(n);
-		return (*head);
-	}
-	
-	temp = *head;
-	while (temp)
-	{
-		if (!temp->next)
-		{
-			new = new_node(n);
-			temp->next = new;
-			break;
-		}
-		temp = temp->next;
-	}
-	return (*head);
-}
+	if (!head)
+		return (0);
 
-/**
- * new_node - creates a new listint_t node.
- * @n: number to place inside the node.
- * 
- * Return: pointer to the new node.
- */
-static listint_t *new_node(const int n)
-{
-	listint_t *new;
-	
-	new = malloc(sizeof(listint_t));
-	if (!new)
-		return (NULL);
-	new->n = n;
-	new->next = NULL;
-	return (new);
+	node = malloc(sizeof(listint_t));
+	if (!node)
+		return (0);
+	node->n = n;
+	node->next = 0;
+
+	last = *head;
+	if (!last)
+	{
+		*head = node;
+		return (node);
+	}
+
+	while (last->next)
+		last = last->next;
+	last->next = node;
+	return (node);
 }
